b20e1956ec258fdff40d7dedd42888940f6c182d
marcelloromani <marcello.romani@smartpension.co.uk>
2022-01-09 04:15:24 +0100

    refactor: split reading static file contents

--- static_files.py
+++ static_files.py
@@ -3,14 +3,21 @@ import os
 STATIC_FILES_DIR = 'static-files'
 
 
-def handler(event, context):
-    path = event["path"]
-    method = event["httpMethod"]
-
+def read_static_file(url_path) -> str:
     # https://stackoverflow.com/a/1945930/1459079
     # if path is absolute, make it relative
-    if os.path.isabs(path):
-        path = "".join(os.path.split(path)[1:])
+    if os.path.isabs(url_path):
+        url_path = "".join(os.path.split(url_path)[1:])
+
+    file_path = os.path.join(os.getenv('LAMBDA_TASK_ROOT') or "", STATIC_FILES_DIR, url_path)
+    with open(file_path, "r") as f:
+        file_content = "".join(f.readlines())
+    return file_content
+
+
+def handler(event, context):
+    url_path = event["path"]
+    method = event["httpMethod"]
 
     if method != 'GET':
         status_code = 400
@@ -18,15 +25,12 @@ def handler(event, context):
         content_type = "text/plain;charset=UTF-8"
 
     else:
-        file_path = os.path.join(os.getenv('LAMBDA_TASK_ROOT'), STATIC_FILES_DIR, path)
-        print(f"File path = {file_path}")
         try:
-            with open(file_path, "r") as f:
-                body = "".join(f.readlines())
+            body = read_static_file(url_path)
             status_code = 200
             content_type = "text/plain;charset=UTF-8"
         except Exception as e:
-            print(f"Error reading {file_path}: {e}")
+            print(f"Error reading {url_path}: {e}")
             body = 'Invalid requeest'
             status_code = 400
             content_type = "text/plain;charset=UTF-8"
@@ -36,3 +40,11 @@ def handler(event, context):
         "body": body,
         "headers": {"Content-Type": content_type},
     }
+
+
+if __name__ == "__main__":
+    res = handler({
+        "path": "/robots.txt",
+        "httpMethod": "GET",
+    }, {})
+    print(res)
